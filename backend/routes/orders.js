76b51e949629ce644887f4e8d5fc256f7d07b1f4
emiliamassing <emiliamassings@gmail.com>
2023-03-20 11:44:25 +0100

    ⬆️📝Installed cors and added neccecary routes

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users');
+  res.send('Orders');
 });
 
 module.exports = router;
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users');
+  res.send('products');
 });
 
 module.exports = router;
