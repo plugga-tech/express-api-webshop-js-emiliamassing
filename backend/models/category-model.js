1b6d8ee93ccd3e7761a42bf0b3f83963091535ce
emiliamassing <emiliamassings@gmail.com>
2023-03-20 14:08:37 +0100

    🎨Small adjustments

--- backend/models/category-model.js
+++ backend/models/category-model.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = mongoose.Schema({
-    categoryName: {
-        type: String
+    name: {
+        type: String,
+        required: true,
+        unique: true
     }
 });
 
