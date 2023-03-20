5a22247c9a010f00b6708182bc81880fb8460abb
emiliamassing <emiliamassings@gmail.com>
2023-03-20 11:47:56 +0100

    ✨Connected to database

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,6 +12,7 @@ const productsRouter = require('./routes/products');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,4 +24,16 @@ app.use('/api/users', usersRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/products', ordersRouter);
 
+async function init() {
+    try {
+        const options = {useNewUrlParser: true, useUnifiedTopology: true};
+        await mongoose.connect('mongodb://127.0.0.1:27017', options)
+        console.log('Connected to database');
+    } catch (err) {
+        console.log("error", err);
+    }
+};
+
 module.exports = app;
+
+init();
