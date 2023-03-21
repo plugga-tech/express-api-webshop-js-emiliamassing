a091a92cf83fb12bf44d60eaecc4b493c1d0d40e
emiliamassing <emiliamassings@gmail.com>
2023-03-21 10:17:57 +0100

    🎨✨Added router to create orders

--- backend/models/order-model.js
+++ backend/models/order-model.js
@@ -3,14 +3,19 @@ const mongoose = require('mongoose');
 const OrderSchema = mongoose.Schema({
     user: {
         type: mongoose.Types.ObjectId,  
-        ref: 'user'
+        ref: 'user',
+        required: true
     },
-    products: {
+    products: [{
+        productId: {
         type: [mongoose.Types.ObjectId],
         ref: 'products',
-        quantity: Number
-    },
-    orderNumer: Number
+        },
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }]
 });
 
 module.exports = mongoose.model('order', OrderSchema);
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const OrderModel = require('../models/order-model');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Orders');
+router.get('/', async function(req, res) {
+  const orders = await OrderModel.find().populate('user');
+  res.status(200).json(orders);
+});
+
+router.post('/add', async function(req, res) {
+  const order = await OrderModel.create(req.body);
+  res.status(201).json(order);
 });
 
 module.exports = router;
