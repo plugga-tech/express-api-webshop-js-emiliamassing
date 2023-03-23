f1dfca951b1530880c9b1eb231777954737ae57f
emiliamassing <emiliamassings@gmail.com>
2023-03-23 16:59:04 +0100

    ⚡️Added error handeling for users router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,44 +5,69 @@ const CryptoJs = require('crypto-js');
 
 /* GET users listing. */
 router.get('/', async function(req, res) {
-  const users = await UserModel.find().select('-password');
+  try {
+    const users = await UserModel.find().select('-password');
 
-  res.status(200).json(users);
+    res.status(200).json(users);
+  } catch (error) {
+    console.log('Error', error);
+    res.status(400).json({message: 'Could not get users'});
+  };
+  
 });
 
 router.post('/userId', async function(req, res) {
-  const {_id} = req.body;
-  const user = await UserModel.findById({_id});
+  try {
+    const {_id} = req.body;
+    const user = await UserModel.findById({_id});
 
-  res.status(200).json(user);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log('Error', error);
+    res.status(400).json({message: 'Could not get user'});
+  };
+  
 });
 
 router.post('/add', async function(req, res) {
-  let newUser = await UserModel.create({
-    username: req.body.username, 
-    email: req.body.email,
-    password: CryptoJs.SHA3(req.body.password).toString()
-  });
+  try {
+    let newUser = await UserModel.create({
+      username: req.body.username, 
+      email: req.body.email,
+      password: CryptoJs.SHA3(req.body.password).toString()
+    });
 
-  console.log("New user", newUser);
-  res.status(201).json(newUser);
+    console.log("New user", newUser);
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log('Error', error);
+    res.status(404).json({message: 'Could not find categories'});
+  };
+
 });
 
 router.post('/login', async function(req, res) {
-  const login = {
-    email: req.body.email,
-    password: req.body.password
-  }
+  try {
+    const login = {
+      email: req.body.email,
+      password: req.body.password
+    }; 
 
-  let findUser = await UserModel.findOne({email: login.email});
-  if(CryptoJs.SHA3(login.password).toString() === findUser.password) {
-    res.status(201).json({email: findUser.email, _id: findUser.id});
+    let findUser = await UserModel.findOne({email: login.email});
+    if(CryptoJs.SHA3(login.password).toString() === findUser.password) {
 
-    console.log(findUser.isLoggedIn);
-    console.log('Login succecssful');
-  } else {
-    res.status(401).json('Incorrect email or password');  
-  }
+      findUser.isLoggedIn = true; 
+      await findUser.save();
+      res.status(201).json({email: findUser.email, _id: findUser.id});
+
+    } else {
+      res.status(401).json('Incorrect email or password');  
+    };
+  } catch (error) {
+    console.log('Error', error);
+    res.status(404).json({message: 'Unexpected error'});
+  };
+  
 });
 
 module.exports = router;
