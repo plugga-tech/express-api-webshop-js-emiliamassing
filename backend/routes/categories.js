ca773ff82640a0208e4fb53903a60f7a99f5d0ac
emiliamassing <emiliamassings@gmail.com>
2023-03-23 16:20:44 +0100

    ⚡️Added error handling

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,25 +3,35 @@ const CategoryModel = require('../models/category-model');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  let categories = await CategoryModel.find();
+router.get('/', async function(req, res) {
+  try{
+    let categories = await CategoryModel.find();
 
-  res.status(200).json(categories);
+    res.status(200).json(categories);
+  } catch(error) {
+    console.log('Error', error);
+    res.status(404).json({message: 'Could not find categories'});
+  }
+ 
 });
 
 router.post('/add', async function(req, res) {
-  let token = req.body.token;
-
-  if(token === process.env.API_TOKEN){
-    let newCategory = await CategoryModel.create({
-      name: req.body.name
-    });
-
-    console.log('New Category added', newCategory);
-    res.status(201).json(newCategory);
-  }else {
-    res.status(401).json({message: "Wrong API-Key"});
+  try {
+    let token = req.body.token;
+    if(token === process.env.API_TOKEN){
+      let newCategory = await CategoryModel.create({
+        name: req.body.name
+      });
+  
+      console.log('New Category added', newCategory);
+      res.status(201).json(newCategory);
+    }else {
+      res.status(401).json({message: "Wrong API-Key"});
+    };
+  } catch (error) {
+    res.status(400).json({message: 'Unexpected error occured'});
   }
+  
 });
 
 module.exports = router; 
